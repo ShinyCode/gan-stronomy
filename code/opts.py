6157133eed91b4701bf4a1dd571d1faab43c659f
ShinyCode <ShinyCode@users.noreply.github.com>
2018-11-19 01:41:26 +0000

    Add run 10.

--- code/opts.py
+++ code/opts.py
@@ -13,22 +13,23 @@ LongTensor = torch.cuda.LongTensor if CUDA else torch.LongTensor
 # Data parameters
 EMBED_SIZE = 1024
 IMAGE_SIZE = 64
-TVT_SPLIT = [0.95, 0.025, 0.025]
+# TVT_SPLIT = [0.95, 0.025, 0.025]
+TVT_SPLIT = [1, 0, 0]
 TVT_SPLIT_LABELS = ['train', 'val', 'test']
-DATASET_NAME = 'data10000'
+DATASET_NAME = 'data1'
 DATA_PATH = os.path.abspath('../temp/%s/data.pkl' % DATASET_NAME)
 
 # Training parameters
-BATCH_SIZE = 32
+BATCH_SIZE = 1
 ADAM_LR = 0.001
 ADAM_B = (0.9, 0.999)
-NUM_EPOCHS = 3
+NUM_EPOCHS = 301
 ALPHA = 0.0004
 
 # Run parameters
-RUN_ID = 9
+RUN_ID = 10
 RUN_PATH = os.path.abspath('../runs/run%d' % RUN_ID)
 IMG_OUT_PATH = os.path.join(RUN_PATH, 'out')
 MODEL_OUT_PATH = os.path.join(RUN_PATH, 'models')
-INTV_PRINT_LOSS = 1
-INTV_SAVE_IMG = 1
+INTV_PRINT_LOSS = 50
+INTV_SAVE_IMG = 50
