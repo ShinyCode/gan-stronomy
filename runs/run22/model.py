9dffee12cffe20674d19d5b844b523c0e1ae0a52
ShinyCode <ShinyCode@users.noreply.github.com>
2018-11-26 02:56:53 +0000

    Add run 22, run on MSE loss only

---
+++ runs/run22/model.py
@@ -0,0 +1,72 @@
+# TODO: perform any necessary preprocessing on x (like scale pixel values to [-1, 1], etc)
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+def padding(f):
+    return (f - 1) // 2
+
+def output_padding(n_in, s, f):
+    p = padding(f)
+    return s * n_in - (n_in - 1) * s + 2 * p - f
+
+def Deconv2dSame(input_size, in_channels, out_channels, kernel_size, stride=1):
+    return nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride, padding=padding(kernel_size), output_padding=output_padding(input_size, stride, kernel_size))
+
+def Conv2dSame(in_channels, out_channels, kernel_size, stride=1):
+    return nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding=padding(kernel_size))
+
+
+# Pytorch uses [N, C, H, W]!
+class Generator(nn.Module):
+    def __init__(self, embed_size, num_classes):
+        super(Generator, self).__init__()
+
+        self.main = nn.Sequential(
+            nn.ConvTranspose2d(embed_size + num_classes, 1024, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(1024),
+            nn.ELU(inplace=True),
+            nn.ConvTranspose2d(1024, 512, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ELU(inplace=True),
+            nn.ConvTranspose2d(512, 256, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ELU(inplace=True),
+            nn.ConvTranspose2d(256, 128, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.ELU(inplace=True),
+            nn.ConvTranspose2d(128, 3, 4, 2, 1, bias=False),
+            nn.Tanh()
+        )
+        
+    # z is (m, 1024), y is (m, n_classes)
+    def forward(self, z, y):
+        zy = torch.cat((z, y), -1)
+        return self.main(zy[:, :, None, None])
+
+class Discriminator(nn.Module):
+    def __init__(self, num_classes):
+        super(Discriminator, self).__init__()
+
+        self.main = nn.Sequential(
+            nn.Conv2d(3, 128, 4, 2, 1, bias=False),
+            nn.ELU(inplace=True),
+            nn.Conv2d(128, 256, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ELU(inplace=True),
+            nn.Conv2d(256, 512, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ELU(inplace=True),
+            nn.Conv2d(512, 1024, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(1024),
+            nn.ELU(inplace=True)
+        )
+        # Layers
+        self.linear = nn.Linear(16384 + num_classes, 1, bias=False)
+
+    # x is (m, 128, 128, 3), y is (m, n_classes)
+    def forward(self, x, y):
+        z = self.main(x).view(-1, 16384)
+        zy = torch.cat((z, y), -1)
+        return torch.sigmoid(self.linear(zy))
---
+++ runs/run22/opts.py
@@ -0,0 +1,40 @@
+import torch
+import os
+
+# General parameters
+SAFETY_MODE = False
+
+# Device parameters
+CUDA = torch.cuda.is_available()
+DEVICE = 'cuda' if CUDA else 'cpu'
+FloatTensor = torch.cuda.FloatTensor if CUDA else torch.FloatTensor
+LongTensor = torch.cuda.LongTensor if CUDA else torch.LongTensor
+
+# Data parameters
+EMBED_SIZE = 1024
+IMAGE_SIZE = 64
+# TVT_SPLIT = [0.95, 0.025, 0.025]
+TVT_SPLIT = [95, 5, 0]
+TVT_SPLIT_LABELS = ['train', 'val', 'test']
+DATASET_NAME = 'data100'
+DATA_PATH = os.path.abspath('../temp/%s/data.pkl' % DATASET_NAME)
+
+# Training parameters
+BATCH_SIZE = 16
+ADAM_LR = 0.001
+ADAM_B = (0.9, 0.999)
+NUM_EPOCHS = 201
+# ALPHA = 0.0004
+
+# Run parameters
+RUN_ID = 22
+RUN_PATH = os.path.abspath('../runs/run%d' % RUN_ID)
+IMG_OUT_PATH = os.path.join(RUN_PATH, 'out')
+# MODEL_PATH = '../runs/run11/models/model_run11_data100_100_0.pt'
+MODEL_PATH = None # None means starting fresh
+MODEL_OUT_PATH = os.path.join(RUN_PATH, 'models')
+INTV_PRINT_LOSS = 1
+INTV_SAVE_IMG = 50
+INTV_SAVE_MODEL = 50
+NUM_UPDATE_G = 1
+NUM_UPDATE_D = 1
