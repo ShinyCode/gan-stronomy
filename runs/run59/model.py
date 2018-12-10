276f40447ff29cff42085098f15834e2524f2bdb
ShinyCode <ShinyCode@users.noreply.github.com>
2018-12-10 20:51:30 +0000

    Add wgan results for run 59

---
+++ runs/run59/model.py
@@ -0,0 +1,59 @@
+# TODO: perform any necessary preprocessing on x (like scale pixel values to [-1, 1], etc)
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import opts
+
+# Pytorch uses [N, C, H, W]!
+class Generator(nn.Module):
+    def __init__(self, latent_size, embed_size):
+        super(Generator, self).__init__()
+            
+        self.main = nn.Sequential(
+            nn.ConvTranspose2d(latent_size + embed_size, opts.NGF * 8, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(opts.NGF * 8),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(opts.NGF * 8, opts.NGF * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(opts.NGF * 4),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(opts.NGF * 4, opts.NGF * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(opts.NGF * 2),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(opts.NGF * 2, opts.NGF, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(opts.NGF),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(opts.NGF, 3, 4, 2, 1, bias=False),
+            nn.Tanh()
+        )
+        
+    # emb is size 1024
+    def forward(self, z, emb):
+        zemb = torch.cat((z, emb), -1)
+        return self.main(zemb[:, :, None, None])
+        
+class Discriminator(nn.Module):
+    def __init__(self, embed_size):
+        super(Discriminator, self).__init__()
+        
+        self.main = nn.Sequential(
+            nn.Conv2d(3, opts.NDF, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(opts.NDF, opts.NDF * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(opts.NDF * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(opts.NDF * 2, opts.NDF * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(opts.NDF * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(opts.NDF * 4, opts.NDF * 8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(opts.NDF * 8),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
+        # Layers
+        self.linear = nn.Linear(4 * 4 * opts.NDF * 8 + embed_size, 1, bias=False)
+
+    # x is (m, 128, 128, 3)
+    def forward(self, x, emb):
+        z = self.main(x).view(-1, 4 * 4 * opts.NDF * 8)
+        zemb = torch.cat((z, emb), -1)
+        return self.linear(zemb)
---
+++ runs/run59/opts.py
@@ -0,0 +1,51 @@
+import torch
+import os
+import util
+
+# General parameters
+SAFETY_MODE = False
+
+# Device parameters
+CUDA = torch.cuda.is_available()
+DEVICE = 'cuda' if CUDA else 'cpu'
+FloatTensor = torch.cuda.FloatTensor if CUDA else torch.FloatTensor
+LongTensor = torch.cuda.LongTensor if CUDA else torch.LongTensor
+torch.manual_seed(0)
+torch.cuda.manual_seed_all(0)
+
+# Data parameters
+EMBED_SIZE = 1024
+LATENT_SIZE = 100
+IMAGE_SIZE = 64
+# TVT_SPLIT = [0.95, 0.025, 0.025]
+TVT_SPLIT = [49800, 100, 100]
+# TVT_SPLIT = [9950, 50, 0]
+# TVT_SPLIT = [950, 50, 0]
+TVT_SPLIT_LABELS = ['train', 'val', 'test']
+DATASET_NAME = 'data50000'
+DATA_PATH = os.path.abspath('../temp/%s/data.pkl' % DATASET_NAME)
+
+# Training parameters
+BATCH_SIZE = 64
+ADAM_LR = 0.0002
+ADAM_B = (0.5, 0.999)
+NUM_EPOCHS = 61
+LAMBDA = 10.0 # Weight of gradient penalty
+
+# Model parameters
+NGF = 64
+NDF = 64
+USE_CLASSES = True
+
+# Run parameters
+RUN_ID = 59
+RUN_COMMENT = 'With latent space and wasserstein loss'
+RUN_PATH = os.path.abspath('../runs/run%d' % RUN_ID)
+IMG_OUT_PATH = os.path.join(RUN_PATH, 'out')
+# MODEL_PATH = '../runs/run11/models/model_run11_data100_100_0.pt'
+MODEL_PATH = None # None means starting fresh
+MODEL_OUT_PATH = os.path.join(RUN_PATH, 'models')
+INTV_PRINT_LOSS = 1
+INTV_SAVE_IMG = 1
+INTV_SAVE_MODEL = 10
+NUM_UPDATE_D = 3
